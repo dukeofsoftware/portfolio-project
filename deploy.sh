26204d5950c92a596c2bdad2c8f828297d8a1d96
DukeEmree <kozanfurkanemre@gmail.com>
2022-09-05 21:04:53 +0300

    build

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/user/bin/env sh
+
+set -e
+
+npm run build
+
+cd dist
+
+git init
+
+git add -A
+git commit -m "New Deployment"
+git push -f git@github.com:DukeEmree/portfolio-project.git master:portfolio-project
+
+cd -
--- src/components/projects/ProjectComponent.vue
+++ src/components/projects/ProjectComponent.vue
@@ -1,6 +1,38 @@
 <template lang="">
-    <div>
-        projects
+    <div class="container">
+        <div class="card-group">
+            <div class="card">
+              <img src="../../assets/discord-logo-logodownload-download-logotipos-1.png" class="card-img-top img-thumbnail" alt="">
+              <div class="card-body">
+                <h5 class="card-title">DukeBot</h5>
+                <p class="card-text">Discord js ile geliştirilmiş bir bot</p>
+              </div>
+              <div class="card-footer">
+                <small class="text-muted">DukeEmree</small>
+              </div>
+            </div>
+        
+            <div class="card">
+              <img src="../../assets/indir.png"  class="card-img-top img-thumbnail" alt="">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Geliştirdiğim todo app</p>
+              </div>
+              <div class="card-footer">
+                <small class="text-muted">DukeEmree</small>
+              </div>
+            </div>
+            <!-- <div class="card">
+              <img src="" class="card-img-top" alt="">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+              </div>
+              <div class="card-footer">
+                <small class="text-muted">Last updated 3 mins ago</small>
+              </div>
+            </div> -->
+          </div>
     </div>
 </template>
 <script>
